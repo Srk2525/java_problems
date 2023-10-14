9968235fe08e73dad2830b01be10adfec20d1b9a
Srk2525 <sanketkanade2525@gmail.com>
2023-10-14 04:51:26 -0700

    first day 1

---
+++ TakingInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class TakingInput {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter ypur licky number");
+        int num_1 = sc.nextInt();
+        System.out.println("your lucky number is: "+ num_1);
+    }
+}
